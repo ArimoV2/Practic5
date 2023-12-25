1b4d96be19c1fcfbad0d0d474e2bc477d7da3da6
KAB-36-1 <KAB-36-1@KAB-35-17->
2023-12-25 09:58:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите название футбольной команды:");
+            //string str = Console.ReadLine();
+            //Console.WriteLine($"{str} - Это Чемпион!");
+            //Console.Read();
+
+
+            //Console.Write("Имя:");
+            //string name = Console.ReadLine();
+            //Console.Write("Фамилия:");
+            //string lastName = Console.ReadLine();
+            //string result = name+" "+lastName;
+            //Console.WriteLine($"{result}");
+            //Console.Read();
+
+
+
+            //Console.Write("Государство:");
+            //string country = Console.ReadLine();
+            //Console.Write("Столица:");
+            //string capital = Console.ReadLine();
+            //string result = "Столица государства "+country + "— город " + capital;
+            //Console.WriteLine($"{result}");
+            //Console.Read();
+
+
+
+            //Console.Write("Роман:");
+            //string novel = Console.ReadLine();
+            //Console.Write("Автор:");
+            //string author = Console.ReadLine();
+            //Console.WriteLine(String.Concat("Писатель ", author, " — автор романа ", novel));
+            //Console.Read();
+
+
+
+            //Console.Write("Число:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Следующее за числом {number1} число – {number1 + 1}. \nДля числа {number1} предыдущее число – {number1 - 1}.");
+            //Console.Read();
+
+
+            //Console.Write("Введите название футбольной команды:");
+            //string str = Console.ReadLine();
+            //Console.WriteLine(str.Length);
+            //Console.Read();
+
+
+
+            //Console.Write("Имя:");
+            //string name = Console.ReadLine();
+            //Console.Write("Сколько лет:");
+            //string age = Console.ReadLine();
+            //Console.WriteLine(String.Concat("Здравствуй, ",name,". Тебе ",age," лет!))"));
+            //Console.Read();
+
+
+
+            //Console.Write("Число:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{number1},{number1+1},{number1+2},{number1*3+3}");
+            //Console.Read();
+
+
+
+            //Console.Write("Число Первое:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.Write("Число Второе:");
+            //int number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Math.Pow(number1, 2) + Math.Pow(number2, 2));
+            //Console.WriteLine(Math.Pow(number1, 2) - Math.Pow(number2, 2));
+            //Console.WriteLine(Math.Pow(number1, 2) * Math.Pow(number2, 2));
+            //Console.WriteLine(Math.Pow(number1, 2) / Math.Pow(number2, 2));
+            //Console.Read();
+
+
+
+
+            //string a = "фальш";
+            //string b = "монеты";
+            //string c = "в";
+            //string d = "ивые";
+            //string e = "мешках";
+            //Console.WriteLine(String.Concat(a,d," ",b," ",c," ",e));
+            //Console.Read();
+
+
+            //string a = "количе";
+            //string b = "информа";
+            //string c = "в байтах";
+            //string d = "ство";
+            //string e = "ции";
+            //Console.WriteLine(String.Concat(a, d, " ", b, e, " ", c));
+            //Console.Read();
+
+
+
+            //string a = "мозг";
+            //string b = "ребусы";
+            //string c = "человече";
+            //string d = "а";
+            //string e = "ского";
+            //Console.WriteLine(String.Concat(b," ", c, e," ", a, d));
+            //Console.Read();
+
+
+
+            string a = "мульти";
+            string b = "технолог";
+            string c = "ийные";
+            string d = "ии";
+            string e = "мед";
+            Console.WriteLine(String.Concat(a, e, c, " ", b, d));
+            Console.Read();
+        }
+    }
+}
